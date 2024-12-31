2d9719746326fe987b73f5f5e0e9a4c52d30645c
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-12-31 23:36:35 +0530

    feat: Integrate Google Maps API and Redux store into main application entry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom";
+import App from "./App";
+import { LoadScript } from "@react-google-maps/api";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import process from "process";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={["places"]}>
+      <App />
+    </LoadScript>
+  </Provider>
+);
